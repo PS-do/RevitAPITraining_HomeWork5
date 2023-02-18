197cf1b303afbd1fef27985c435932a84cea96bf
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-18 20:14:24 +0300

    ed

---
+++ RevitAPITraining_HomeWork5_Ribbon/Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_HomeWork5_Ribbon
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {        
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //Для двух приложений выше создайте вкладку на ленте в Revit, панель и 2 кнопки.
+            string tabName = "RevitAPITrainig";
+            application.CreateRibbonTab(tabName);
+            string utilsFloberPath = @"C:\Program Files\RevitAPITrainig\";
+            var panel = application.CreateRibbonPanel(tabName, "Задание 5.3");
+            var button = new PushButtonData("5.1", "Кнопка на выполнение программы задания 5.1",
+                Path.Combine(utilsFloberPath, "RevitAPITraining_Task5_1.dll"),
+                "RevitAPITraining_Task5_1.Main");
+            panel.AddItem(button);
+            button = new PushButtonData("5.2", "Кнопка на выполнение программы задания 5.2",
+                Path.Combine(utilsFloberPath, "RevitAPITraining_Task5_2.dll"),
+                "RevitAPITraining_Task5_2.Main");
+            panel.AddItem(button);
+            return Result.Succeeded;
+        }
+    }
+}
